52db1d4983dfee29e8ef142ed76d24978734fd44
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-08-02 11:11:13 -0500

    Borrando practica de bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 // Este es el punto de entrada de tu aplicacion
-//Prueba boton
-document.getElementById('login').addEventListener('click', () => {
-alert('funciona tambien');
-});
-alert('funciona');
-
 import { myFunction } from './lib/index.js';
 
 myFunction();
