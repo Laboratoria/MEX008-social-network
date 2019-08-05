26cf0880200e1343e01154d227ee247e91caa919
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-08-05 13:56:51 -0500

    Subo configuracion para firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 // Este es el punto de entrada de tu aplicacion
-
+/* 
 import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction(); */
+
+const data = firebase.database();
+
+let data = {
+    "Moscow": {
+      country: "Russia"
+    },
+    "Berlin": {
+      name: "Germany"
+    }
+  }
+
+  let dataRef = database.ref('cities');
+  let dataPush = dataRef.push(data);
+  dataRef.once('value', snapshot => {
+    console.log(snapshot.val());
+  });
