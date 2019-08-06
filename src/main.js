5cdc61d731c626fe647cc69ae1f6fcafa799898c
CassandraGarell <cassandragarell@gmail.com>
2019-08-06 18:43:10 -0500

    Subiendo primer maqueta con modal de inicio de sesión de la primera historia de usuario

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.modal');
+    let instances = M.Modal.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.autocomplete');
+    let instances = M.Autocomplete.init(elems);
+  });
