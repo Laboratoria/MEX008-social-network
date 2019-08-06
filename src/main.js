d840681adcd9b8c947ffd4eb6a3a711869da2da2
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-08-06 00:32:49 -0500

    Subo archivo main. js con funcion qpara crear un nuevo usuario con contraseña y password

--- src/main.js
+++ src/main.js
@@ -4,19 +4,48 @@ import { myFunction } from './lib/index.js';
 
 myFunction(); */
 
-const data = firebase.database();
+/* Declaracion de variables */
+let buttonSignIn = document.getElementById('button-sign-in');
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+/*Declaracion de funciones*/
 
-let data = {
-    "Moscow": {
-      country: "Russia"
-    },
-    "Berlin": {
-      name: "Germany"
+
+function handleSignUp() {
+    console.log("diste un clic")
+    console.log(email);
+    console.log(password);
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
     }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    } 
+    /*Funcion para crear un nuevo usuario */
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error); 
+      // [END_EXCLUDE]
+     }); 
+    // [END createwithemail] 
   }
 
-  let dataRef = database.ref('cities');
-  let dataPush = dataRef.push(data);
-  dataRef.once('value', snapshot => {
-    console.log(snapshot.val());
-  });
+/*Declaracion de eventos*/   
+buttonSignIn.addEventListener('click',handleSignUp);
+
+  
