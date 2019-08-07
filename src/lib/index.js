3c316f65467063e2a954cbbf1355dab7d7476c71
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-08-07 17:18:47 -0500

    Cambio despues de sincronizacion

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,37 +3,3 @@ alert ("Aqui estoy :)");
 export const myFunction = () => {
   console.log('Hola mundo!');
 };
-
-document.getElementById('iniciar-sesion').addEventListener('click', () => {
-  Swal.fire({
-    title: '<i class="fas fa-user-alt"></i>',
-    html:
-      '<button class = btn-social face><img  class= img-social src= "img/googleicon.png"> Ingresa con Google</button>' + '<br>' +
-      '<button class= btn-social googl><img class= img-social src= "img/facebookicon.png"> Ingresa con Facebook</button>'+ '<br>' + '<br>' +
-      '<input type="email" placeholder="Email">' + '<br>' +
-      '<input type="password" placeholder="Password">' + '<br>'+ '¿Olvidaste tu contraseña?' + '<br>' + '<button class="btn-warning">Registrate</button>',
-    showCloseButton: true,
-    focusConfirm: false,
-    confirmButtonText:
-      '<i class="fa fa-thumbs-up"></i> Acceder!',
-    confirmButtonAriaLabel: 'Registrate!',
-  })
-});
-
-document.getElementById('registrate').addEventListener('click', () => {
-  Swal.fire({
-    title: '/MEX008-social-network/src/img/login.png',
-    html:
-      '<input type="email" placeholder="Email"> ' +
-      '<input type="password" placeholder="Password">' + '<br>'+ '¿No tienes una cuenta?' + '<br>' + '<button class="btn-warning">Registrate</button>',
-    showCloseButton: true,
-    showCancelButton: true,
-    focusConfirm: false,
-    confirmButtonText:
-      '<i class="fa fa-thumbs-up"></i> Acceder!',
-    confirmButtonAriaLabel: 'Registrate!',
-    cancelButtonText:
-      '<i class="fa fa-thumbs-down"></i>',
-    cancelButtonAriaLabel: 'Cancelar',
-  })
-});
