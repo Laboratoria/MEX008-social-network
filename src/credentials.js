82e8153c2b0de3a58f434a206b8534909d922305
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-08-07 16:31:31 -0500

    agregando menu de hamburguesa que funciona

--- src/credentials.js
+++ src/credentials.js
@@ -32,7 +32,7 @@
    },
    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
    signInFlow: 'popup',
-   signInSuccessUrl: './#muro',
+   signInSuccessUrl: './#timeline',
    signInOptions: [
      // Leave the lines as is for the providers you want to offer your users.
      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
