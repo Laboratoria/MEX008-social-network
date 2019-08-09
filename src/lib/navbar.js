52700c6ddbaa0a6c8fcac3e48ae183a80d7df305
Isis Lázaro <51394610+isisLazaro@users.noreply.github.com>
2019-08-08 20:06:56 -0500

    Update navbar.js

--- src/lib/navbar.js
+++ src/lib/navbar.js
@@ -3,7 +3,7 @@ let Navbar = {
         let view = /*html*/`
             <nav id = "navbar-dsk" class = "navbar">
                 <a href = "./#/"><img src = "imgs/Logo-color.png" id = "logo"></a><br>
-                <a href = "./#/login" class = "linksToJoin" >Incia sesión</a>
+                <a href = "./#/login" class = "linksToJoin" >Inicia sesión</a>
                 <a href = "./#/register" class = "linksToJoin"> Crea tu cuenta</a>
             </nav>
             <!--  mobile version -->
