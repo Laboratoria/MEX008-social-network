e0501a3503f27d7052a72ecb48514ef40c669431
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-08-09 19:34:17 -0500

    Nodo didactico

--- src/main.js
+++ src/main.js
@@ -3,41 +3,41 @@ import { myFunction } from './lib/index.js';
 
 myFunction();
 
-//A
-const startButton = document.getElementById('start-button');
+// //A
+// const startButton = document.getElementById('start-button');
 
-//A-Comienzo haciendo una función (Sigue A)
-const nodoOfPresentationMobile = () => {
-    //console.log('Funciona');
+// //A-Comienzo haciendo una función (Sigue A)
+// const nodoOfPresentationMobile = () => {
+//     //console.log('Funciona');
 
-    //Creando nodo del dom solo para presentación de vista mobile
+//     //Creando nodo del dom solo para presentación de vista mobile
 
-    //Identificar primer padre
-    let nodoPadre = document.getElementsByTagName('section')[0];
+//     //Identificar primer padre
+//     let nodoPadre = document.getElementsByTagName('div')[0];
 
-    console.log(nodoPadre);
+//     console.log(nodoPadre);
 
-    //Identificar hijo
+//     //Identificar hijo
 
-    let primerHijo = document.getElementsByTagName('div')[1];
-    console.log(primerHijo);
+//     let primerHijo = document.getElementsByTagName('section')[0];
+//     console.log(primerHijo);
 
-    //Eliminar hijo antes de insertar nuevo
-    nodoPadre.removeChild(primerHijo);
+//     //Eliminar hijo antes de insertar nuevo
+//     nodoPadre.removeChild(primerHijo);
 
-    //Imprimir nuevo nodo
-    //Paso 1: Crear elementos
-    let nuevoElemento = document.createElement('div');
-    //Paso 2: Crear tipo de nodo
-    let textoDelElemento = document.createTextNode('Prueba');
-    //Paso 3: Prueba de acceso
-    let pruebaSegundoElemento = document.getElementsByTagName('div')[1];
-    console.log(pruebaSegundoElemento);
-    //Paso 4: Ligar padre con hijo
-    nuevoElemento.appendChild(textoDelElemento);
-    nuevoElemento.setAttribute('id','showPresentation');
-};
+//     //Imprimir nuevo nodo
+//     //Paso 1: Crear elementos
+//     let nuevoElemento = document.createElement('section');
+//     //Paso 2: Crear tipo de nodo
+//     let textoDelElemento = document.createTextNode('Prueba');
+//     //Paso 3: Prueba de acceso
+//     let pruebaSegundoElemento = document.getElementsByTagName('div')[1];
+//     console.log(pruebaSegundoElemento);
+//     //Paso 4: Ligar padre con hijo
+//     nuevoElemento.appendChild(textoDelElemento);
+//     nuevoElemento.setAttribute('id','showPresentation');
+// };
 
-//Eventos que escuchan un click
-//A
-startButton.addEventListener('click', nodoOfPresentationMobile);
+// //Eventos que escuchan un click
+// //A
+// startButton.addEventListener('click', nodoOfPresentationMobile);
