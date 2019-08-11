e7ee03d4bdd8d79c9128dbfb9574309b740dec2b
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-08-11 12:21:17 -0500

    Subo correccion para la funcion de iniciar con google

--- src/lib/index.js
+++ src/lib/index.js
@@ -105,7 +105,6 @@ const emailPasswordLogIn = (emailLogin,passwordLogIn) => {
 
 // Ingreso por Google //
 function googleSignIn() {
-    if (!firebase.auth().currentUser) {
     console.log('funciona google')
     // [START createprovider]
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -132,7 +131,7 @@ function googleSignIn() {
         console.log(errorMessage);
       }
     });
-  }
+  
 }
 
 // Ingreso por facebook //
