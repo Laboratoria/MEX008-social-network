7e2169d2a4a5cbdce18dd401dbf6f210af417b13
Isis Lázaro <isislazaro@gmail.com>
2019-08-12 22:02:21 -0500

    se cambió estructura del html para cambiar estilos

--- src/lib/home.js
+++ src/lib/home.js
@@ -1,7 +1,8 @@
 let Home = {
     render : async () => {
         let view = /* html */ `
-            <figure id = "home-background" class = "page-background">
+            <section id = "home">
+                <figure id = "home-background" class = "page-background"></figure>
                 <main id = "main-home-mbl" class = "main-home"> 
                     <h1>El espacio incluyente donde toda la comunidad de bodas se conecta.</h1><br>
                     <a href = "./#/login" id = "btn-login" class = "linksToJoin" >Inicia sesión</a><br><br>
@@ -10,9 +11,8 @@ let Home = {
                 <main id = "main-home-dsk" class = "main-home"> 
                     <h1>El espacio incluyente donde toda la comunidad de bodas se conecta.</h1><br><br>
                     <p>Planear una boda puede ser muy complicado, quieres que sea perfecta, lo que siempre has soñado, pero a la vez que se ajuste a la realidad.</p>
-                    <p>Es por eso que existe Together-Click, un espacio donde todas aquellas personas interesadas en casarse se encuentran, se apoyan, comparten dudas, ideas, consejos y pueden involucrar a sus seres queridos en su gran día. </p>
                 </main>
-            </figure>
+            </section>    
         `
         return view
     },
