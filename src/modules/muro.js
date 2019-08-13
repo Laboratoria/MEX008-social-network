5c971ecbeafd33c4c9ea80bc347a982db0c3018d
TeresaFlores <teresaflores1730@gmail.com>
2019-08-13 15:14:03 -0500

    Nueva version SPA y LOGIN

--- src/modules/muro.js
+++ src/modules/muro.js
@@ -1,7 +1,10 @@
 let Muro = {
     render : async () => {
         let view = /* html */ `
+        <div id="muro">
+
             <h1>HOLA ESTE SERA EL MURO</h1>
+        </div>    
         `
         return view
     },
