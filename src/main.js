daa2d780b1daea8ee4697195e17111746831eee9
Laboratoria008 <jessicazetina37@gmail.com>
2019-08-17 23:00:08 -0500

    Terminando proyecto se agrego muro con publicacion

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ const registro = (e) => {
     let password= document.getElementById('password').value;
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then (function(){
+        verificar()
         console.log("registrado");
-        // location.hash = '/crear-perfil'
-        // enviarCrearPerfil()
-        location.hash = '/muro'
     })
     .catch(function(error) {
         // Handle Errors here.
@@ -15,21 +13,24 @@ const registro = (e) => {
         const errorMessage = error.message;
         console.log(errorCode);
         console.log(errorMessage);
-        // ...
+        document.getElementById("alertas").innerHTML=errorCode;
+        document.getElementById("alertas").innerHTML = errorMessage;
+         // ...
     });email-password.html
 
 };
 
 //comentario 
 
-const ingreso= ()=>{
-let email2= document.getElementById('email2').value;
-let password2= document.getElementById('password2').value;
-
-firebase.auth().signInWithEmailAndPassword(email2, password2)
-.then (function(){
-    console.log("Ingreso de usuario viejo");
-     enviarMuro();
+const ingreso= (e)=>{
+    e.preventDefault();
+   let email2= document.getElementById('email2').value;
+   let password2= document.getElementById('password2').value;
+   
+   firebase.auth().signInWithEmailAndPassword(email2, password2)
+   .then (function(){
+    console.log("Ingreso de usuario que ya estaba registrado");
+    location.hash = '/muro'
 })
 .catch(function(error) {
     // Handle Errors here.
@@ -37,6 +38,8 @@ firebase.auth().signInWithEmailAndPassword(email2, password2)
     const errorMessage = error.message;
     console.log(errorCode);
     console.log(errorMessage);
+    document.getElementById("alertas2").innerHTML = errorCode;
+    document.getElementById("alertas2").innerHTML = errorMessage;
     
     // ...
 });email-password.html
@@ -47,16 +50,19 @@ const observador= ()=> {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
         console.log("existe usuario registrado");
-        
+        if(user.emailVerified) {
+            location.hash = '/muro'
+            console.log("ya paso")
+        }
           // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          console.log(user);
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
+          const displayName = user.displayName;
+          const email = user.email;
+          console.log(user.emailVerified);
+          const emailVerified = user.emailVerified;
+          const photoURL = user.photoURL;
+          const isAnonymous = user.isAnonymous;
+          const uid = user.uid;
+          const providerData = user.providerData;
           // ...
         } else {
             console.log("No existe usuario registrado");
@@ -64,19 +70,11 @@ const observador= ()=> {
           // ...
         }
       });
-               
-}
-observador();
 
-const enviarMuro=()=>{
- location.hash = '/muro'
-};
+ }
+ observador();
 
-const enviarCrearPerfil=()=>{
-    location.hash = '/crear-perfil'
-   };
-
-const cerrar=() => {
+ const cerrar=() => {
     firebase.auth().signOut()
     .then (function(){
         console.log("Saliendo...");
@@ -85,6 +83,56 @@ const cerrar=() => {
     .catch(function(error){
         console.log(error);
     })
+ };
+
+ const verificar= () =>{
+    let user = firebase.auth().currentUser;
+        
+    user.sendEmailVerification()
+        .then(function() {
+        // Email sent.
+        console.log("enviando correo")
+        })
+        .catch(function(error) {
+        // An error happened.
+        console.log("error de correo")
+        });
+ }
+
+ const mensajesError= () => {
+    if ( registro == 'errorCode' || registro ==  'errorMessage' ){
+        document.getElementById('alerta');
+        alerta.addEventListener('click', mensajesError)
+    }
+ }
+
+ const google= () => {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then (function(user){
+            console.log(user);
+
+        })
+        .catch(function(error){
+           console.log(error);
+        })
+
+ }
+
+
+//carga de imagen en  muro //
+const cargaArchivo = async (e) => { 
+    let imagenesBaseDatos = firebase.database().ref('Imagenes/'+document.getElementById("email"));
+   alert("Subiendo Imagen: "+e.target.value);
+   let file = e.target;
+    if(file.files && file.files[0]){
+        imagenesBaseDatos.push({
+            urlImage:file.files[0],
+            email:document.getElementById("email")
+        });
+       alert("Carga Exitosa");
+      }else{
+        alert("Carga No Exitosa");
+      }
+      
 };
-
-
