1857ea06a2de42f2cde896b26a0e37aea5f6255e
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-08-19 12:24:00 -0500

    Subiendo cambios

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,12 +5,17 @@ let Navbar = {
             <figure>
                 <img src="img/logo.svg" alt="logo">
             </figure>
+            <div>
+                <figure id=photo-head ></figure>
+                <span id="name-head"></span>
+            </div>
         </nav>`
       
     return view
 },
 
 after_render: async () => {}
+
 
 }
 
