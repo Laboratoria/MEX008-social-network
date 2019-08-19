b4dd6a26666563726bf2876e5836404d632e6269
LedaHuerta <ledamhuerta@gmail.com>
2019-08-19 11:34:03 -0500

    actualizando (printPost):pasandole el id unico de cada post, actualizando(editPost):pasandole el id unico para solucionar (issue):resuelto, ya actualiza post por post

--- src/components/post-controller.js
+++ src/components/post-controller.js
@@ -5,7 +5,7 @@ const printPost = user => {
     user.forEach(msj => {
         let post = msj.data();
         if (post.usuario == userUid.uid) {
-            // console.log('ya sirvooooo')
+            console.log(post.postId)
             let card = `
                             <div class="col s12 m7 box-post">
                              <div class="head-post">
@@ -16,10 +16,12 @@ const printPost = user => {
                                     <p class="disp-name">${post.nombre}</p>
                                     <!--<p>${post.hora}</p>-->
                                     <div id="posttxt"class="posttxt">
-                                    <input id="post-txt-edit" placeholder="Editar post">${post.postContent}</input>
+                                    <input id="${post.postId}" placeholder="Editar post">${post.postContent}</input>
                                     <p id="issue-edit-post"></p>
                                     </div>
-                                    <div class="admin-post"><a data-target="modal2" id="btn-edit"><i class="material-icons">edit</i></a>
+                                    <div class="admin-post">
+                                    <a class="btns-edit btn-edit" id="${post.postId}"><i id="${post.postId}" class="material-icons">edit</i></a>
+                                    <a id="btn-delete"><i class="material-icons">delete</i></a>
                                     </div>
                                     </div>
                                 </div>
@@ -27,8 +29,9 @@ const printPost = user => {
                             </div>
                             `
             rootPost.insertAdjacentHTML("afterbegin", card);
-            btnEdit = document.getElementById('btn-edit');
-            btnEdit.addEventListener('click', editPost);
+            // btnEdit = document.getElementById('btn-edit');
+            // btnEdit.addEventListener('click', editPost);
+
 
             // btnEdit.addEventListener('DOMContentLoaded', function() {
             //     console.log('modal')
@@ -54,10 +57,63 @@ const printPost = user => {
             rootPost.insertAdjacentHTML("afterbegin", card)
         }
     })
+    const btnsEdit = document.getElementsByClassName('btns-edit');
+
+    for (let i = 0; i < btnsEdit.length; i++) {
+        btnsEdit[i].addEventListener('click', () => {
+            let postId = event.target.id
+            editPost(postId);
+        })
+    }
+
 }
 
-let editPost = () => {
+let editPost = (postId) => {
     // console.log('funcion editando')
+    // Asignamos un 'Guard' que indique si un usuario esta logueado y toda la informacion que tenemos de el
+    firebase.auth().onAuthStateChanged((user) => {
+        // User is signed in.
+        if (user.uid) {
+            // console.log(user);
+            // Declaramos el input de edicion y obtenemos su valor
+            let postTxt = document.getElementById(postId).value;
+            let getPost = db.collection("post").doc(postId);
+            getPost.get().then(function(doc) {
+                if (doc.exists) {
+                    if (postTxt != '') {
+                        // console.log(doc.data().postId);
+                        console.log("Document data:", doc.data());
+                        //creamos una funcion que actualice el post a la base de datos en firestore con los campos: usuario, nombre, post content y hora
+                        db.collection("post").doc(postId).update({
+                                usuario: user.uid,
+                                nombre: user.displayName,
+                                postContent: postTxt,
+                                hora: new Date(),
+                                edit: 'Editado'
+                            }).then(function(docRef) {
+                                console.log("Document successfully updated!");
+                            })
+                            .catch(function(error) {
+                                console.error("Error writing document: ", error);
+                            });
+                    } else {
+                        const issueEditRoot = document.getElementById('issue-edit-post');
+                        issueEditRoot.innerHTML = 'Por favor escribe algo';
+                    }
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+            // console.log(postTxt);
+
+        }
+    });
+}
+
+let deletePost = () => {
     // Asignamos un 'Guard' que indique si un usuario esta logueado y toda la informacion que tenemos de el
     firebase.auth().onAuthStateChanged((user) => {
         // User is signed in.
@@ -100,6 +156,7 @@ let editPost = () => {
 
         }
     });
+
 }
 
 
